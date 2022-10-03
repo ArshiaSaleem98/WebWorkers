66190ed73e2dae85bb68953799e43cfe019a987c
ArshiaSaleem98 <arshiasaleem98@gmail.com>
2022-10-03 10:49:38 +0100

    feat:webWorkers

---
+++ script.js
@@ -0,0 +1,25 @@
+const worker = new Worker("worker.js");
+
+
+const sumButton = document.getElementById("sumButton");
+const backgroundButton = document.getElementById("backgroundButton");
+sumButton.addEventListener("click", function () {
+  let sum = 0;
+  for (let index = 0; index < 10000000000000; index++) {
+    sum += index;
+  }
+  alert(`total sum ${sum}`);
+});
+
+
+// worker.onmessage= function(message){
+//   console.log(message)
+// }
+
+backgroundButton.addEventListener("click", function () {
+  if (document.getElementById("container").style.backgroundColor != "blue") {
+    document.getElementById("container").style.backgroundColor = "blue";
+  } else {
+    document.getElementById("container").style.backgroundColor = "green";
+  }
+});
